e9b90dd822d34d725721df3cf7332ff88597d33a
Ephantus karanja <ephantusslogan@gmail.com>
2021-06-15 08:27:22 +0300

    add required pass in login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -31,6 +31,7 @@ class LoginController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'username' => 'required',
+            'password' => 'required',
         ]);
 
 
