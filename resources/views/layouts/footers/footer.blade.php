8c24d660178b44123e6bc2317a285c7db661d531
Ephantus karanja <ephantusslogan@gmail.com>
2021-06-21 13:49:42 +0300

    modify footer

--- resources/views/layouts/footers/footer.blade.php
+++ resources/views/layouts/footers/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      <img src="{{ asset('assets/img/choice1.png')}}" alt="FCL Calibra Logo" class=" brand-image" style="height: 35px;">
+      <img src="{{ asset('assets/img/choice1.png')}}" alt="CML Calibra Logo" class=" brand-image" style="height: 35px;">
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; <?php echo date('Y')?>. <a href="https://adminlte.io" target="_blank" style="color: green"> Choice Meats Halal Ltd</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; <?php echo date('Y')?>. <a href="https://www.choicemeats.co.ke/" target="_blank" style="color: green"> Choice Meats Halal Ltd</a>.</strong> All rights reserved.
   </footer>
