2156ccca8105e79d94e2c2be52b2deb3849643c7
Ephantus karanja <ephantusslogan@gmail.com>
2021-09-16 14:01:01 +0300

    key

--- resources/views/slaughter/weigh.blade.php
+++ resources/views/slaughter/weigh.blade.php
@@ -668,8 +668,8 @@
             var side_A = $('#side_A').val();
             var side_B = $('#side_B').val();
 
-            if (!(side_A >= 5 && side_A <= 200) || !(side_B >= 5 && side_B <= 200)) {
-                alert("Please ensure you have valid weight of between 5-200 kgs in both sides.");
+            if (!(side_A >= 5 && side_A <= 250) || !(side_B >= 5 && side_B <= 250)) {
+                alert("Please ensure you have valid weight of between 5-250 kgs in both sides.");
                 return false;
             }
         }
