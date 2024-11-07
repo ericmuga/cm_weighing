c202fe739bb519cfee5da98b8c2370286095b305
Adiela Abishua <adiela.abishua@gmail.com>
2024-11-07 15:22:19 +0300

    Create table and model for items

---
+++ database/migrations/2024_11_07_114906_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique(); // stands for both unique and indexable
+            $table->string('barcode');
+            $table->string('description', 50);
+            $table->string('category', 50)->nullable();
+            $table->string('unit_of_measure', 10)->nullable();
+            $table->double('qty_per_unit_of_measure')->default(0);
+            $table->integer('unit_count_per_crate')->default(0);
+            $table->tinyInteger('blocked')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
