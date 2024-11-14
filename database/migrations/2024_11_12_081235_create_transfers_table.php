9a9428e24ce63f9cf8f97d6912357da31c895d87
Adiela Abishua <adiela.abishua@gmail.com>
2024-11-14 08:23:37 +0300

    Transfers table: add recieving columns

--- database/migrations/2024_11_12_081235_create_transfers_table.php
+++ database/migrations/2024_11_12_081235_create_transfers_table.php
@@ -26,6 +26,9 @@ class CreateTransfersTable extends Migration
             $table->string('narration', 255)->nullable();
             $table->boolean('manual_weight')->default(0);
             $table->foreignId('user_id')->constrained('users');
+            $table->double('received_weight', 8, 2);
+            $table->integer('received_pieces')->nullable();
+            $table->foreignId('received_by')->constrained('users');
             $table->timestamps();
         });
     }
