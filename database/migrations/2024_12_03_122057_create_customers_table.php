055ba250123f02eac6da7a43f565886a37ec4473
Adiela Abishua <adiela.abishua@gmail.com>
2024-12-05 08:42:16 +0300

    Customers: create database table and model

---
+++ database/migrations/2024_12_03_122057_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number', 10);
+            $table->string('location', 50)->nullable();
+            $table->string('kra_pin', 11)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
