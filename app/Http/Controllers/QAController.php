5a856935fe65d82c0c700ce893bd0d2364c76a68
Adiela Abishua <adiela.abishua@gmail.com>
2024-12-13 12:13:36 +0300

    Update QAController.php
    Remove unused import

--- app/Http/Controllers/QAController.php
+++ app/Http/Controllers/QAController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Helpers;
 use Brian2694\Toastr\Facades\Toastr;
-use Hamcrest\Type\IsString;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
