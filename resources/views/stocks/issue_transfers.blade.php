7e132c1f7879eabe4d9a73adb7973908f6c8918b
Ephantus Karanja <ephantusslogan@gmail.com>
2024-12-30 12:28:35 +0300

    update scale route

--- resources/views/stocks/issue_transfers.blade.php
+++ resources/views/stocks/issue_transfers.blade.php
@@ -220,7 +220,7 @@
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]')
                         .attr('content')
                 },
-                url: "{{ url('slaughter/read-scale') }}",
+                url: "{{ route('read_scale') }}",
 
                 data: {
                     'comport': comport,
